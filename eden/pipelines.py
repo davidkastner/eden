3965996112b4ad88fded5971e283e0e7f31dc63e
chuckthepiranha <stephjoan1@me.com>
2022-09-08 10:22:01 -0400

    remove unused variables for the time being

--- eden/pipelines.py
+++ eden/pipelines.py
@@ -34,11 +34,11 @@ def basic_pipline() -> None:
     # Clean the downloaded raw geodata
     geodata_df = process.clean_geodata(raw_geodata_df)
     # Generate base working df from the intersection of all dataframes
-    base_df = process.geodata_intersect(county_df, city_df, geodata_df)
+    process.geodata_intersect(county_df, city_df, geodata_df)
     # Clean and simplify download drought data
-    drought_df = process.clean_drought()
+    process.clean_drought()
     # Append congessional districts column
-    base_df = collect.get_congressional_districts()
+    collect.get_congressional_districts()
 
     print("\nEden terminated.")
 
