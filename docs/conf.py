dd8889eaa5657411099170503d5d1029ab7e70e5
chuckthepiranha <stephjoan1@me.com>
2023-05-08 10:50:53 -0400

    update version again

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ copyright = ("2022, David W. Kastner")
 author = 'David W. Kastner'
 
 # The short X.Y version
-version = '6.0'
+version = ''
 # The full version, including alpha/beta/rc tags
 release = ''
 
