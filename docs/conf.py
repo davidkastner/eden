6630f70aaa11a60f564e0e7ad3fc6c87ffa57cd9
chuckthepiranha <stephjoan1@me.com>
2024-01-07 08:26:41 -0500

    remove unused imports

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 # Incase the project was not installed
-import revitron_sphinx_theme
-import eden
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -86,7 +84,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
