bc3bbcf32092a4eb927c9ebc3737a49828bbb4e1
chuckthepiranha <stephjoan1@me.com>
2024-01-08 07:39:39 -0500

    try more stuff

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,6 @@ extensions = [
 ]
 
 autosummary_generate = True
-autoapi_generate_api_docs = False
 autoapi_type = 'python'
 autoapi_dirs = ['../eden']
 autoapi_ignore = ["*/tests/*"]
@@ -143,7 +142,9 @@ html_context = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ['custom.css']
+html_js_files = []
 html_logo = '_static/logo-white.svg'
 
 # Custom sidebar templates, must be a dictionary that maps document names
